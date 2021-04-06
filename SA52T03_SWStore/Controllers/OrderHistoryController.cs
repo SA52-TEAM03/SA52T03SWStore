5e30ed1c407725344e15e429a4f079cd5f3853d5
kyawmt <kmt@kmtkko.com>
2021-04-06 13:06:03 +0800

    Add Order History and Cart Controller
    (cherry picked from commit 262b66d69863b7df69bdc8468b8044b5b8aa5762)

---
+++ SA52T03_SWStore/Controllers/OrderHistoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA52T03_SWStore.Controllers
+{
+    public class OrderHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
