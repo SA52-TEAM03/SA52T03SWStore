499a5e815695988e64467ab38f95182bb52e7557
andyswj <e0696682@u.nus.edu>
2021-04-11 07:31:06 +0800

    HomePage BackEnd: Added Search and SearchByCategoeries Method. Added Search View

--- SA52T03_SWStore/Controllers/HomeController.cs
+++ SA52T03_SWStore/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SA52T03_SWStore.Controllers
@@ -16,6 +17,7 @@ namespace SA52T03_SWStore.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public HomeController(ApplicationDbContext db)
         {
             _db = db;
@@ -32,6 +34,79 @@ namespace SA52T03_SWStore.Controllers
             return View(homePageViewModel);
         }
 
+        //Search Method
+        [HttpPost]
+        public IActionResult Search(string word)
+        {
+            if(word == null)
+            {
+                var searchProduct = from p in _db.Product
+                                    select p;
+
+                ViewData["SearchProducts"] = searchProduct;
+                return View();
+            }
+            else
+            {
+                //To retrive products with the search word
+                var searchProduct = from p in _db.Product
+                                    where p.Description.Contains(word) || p.Name.Contains(word)
+                                    select p;
+
+                ViewData["SearchProducts"] = searchProduct;
+
+                return View();
+            }
+        }
+
+        //Search by Category Method
+        public IActionResult SearchCat(int id)
+        {
+            //To retrive products from the category
+            var iter = from p in _db.Product
+                       where p.CategoryId == id
+                       select p;
+
+            ViewData["SearchProducts"] = iter;
+
+            return View("Search");
+        }
+
+        public async Task<IActionResult> AddToCart(int id)
+        {
+
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var productFromDb = await _db.Product.Where(m => m.Id == id).FirstOrDefaultAsync();
+
+
+            ShoppingCart shoppingCart = new ShoppingCart
+            {
+                CustomerId = claim.Value,
+                Product = productFromDb,
+                ProductId = productFromDb.Id,
+                Quantity = 1
+            };
+
+            ShoppingCart cartFromDb = await _db.ShoppingCart.Where(c => c.CustomerId == shoppingCart.CustomerId
+                                                && c.ProductId == shoppingCart.ProductId).FirstOrDefaultAsync();
+
+            if (cartFromDb == null)
+            {
+                await _db.ShoppingCart.AddAsync(shoppingCart);
+
+            }
+            else
+            {
+                cartFromDb.Quantity++;
+            }
+            await _db.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+
+        }
         public IActionResult Privacy()
         {
             return View();
