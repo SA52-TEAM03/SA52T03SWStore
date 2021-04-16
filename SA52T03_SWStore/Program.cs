2ad0a671feab185295991b4ef2ca8501f472a0ff
kyawmt <kmt@kmtkko.com>
2021-04-16 14:26:08 +0800

    Update on Seeding Product Data

--- SA52T03_SWStore/Program.cs
+++ SA52T03_SWStore/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using SA52T03_SWStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,24 +13,7 @@ namespace SA52T03_SWStore
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
