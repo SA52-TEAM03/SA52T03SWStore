8281f1cf3f5035681c54b8cb11dc411bdbe864ba
zouxunlong <449326540@qq.com>
2021-04-17 16:12:27 +0800

    homepage add-in-cart login jump done.

--- SA52T03_SWStore/wwwroot/js/cart.js
+++ SA52T03_SWStore/wwwroot/js/cart.js
@@ -12,32 +12,14 @@ function onClick1(event) {
     let productid = elem.getAttribute("productId");
     let xhr = new XMLHttpRequest();
 
-    //let el = document.createElement("div");
-    //el.setAttribute("style", "position:fixed;top:70px;right:70px;");
-    //el.className = "alert alert-success btn-sm"
-    //el.innerHTML = "Successfully added to Cart";
-    //setTimeout(function () {
-    //    el.parentNode.removeChild(el);
-    //}, 3000);
-    //document.body.appendChild(el);
-
     xhr.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             let data = JSON.parse(this.responseText);
-
-            let notification = document.createElement("div");
-            notification.setAttribute("style", "position:fixed;top:70px;right:70px;");
-            notification.className = "alert alert-success btn-sm"
-            notification.innerHTML = "Successfully added to Cart";
-            setTimeout(function () {
-                notification.parentNode.removeChild(notification);
-            }, 2000);
-            document.body.appendChild(notification);
             document.getElementById("shoppingCartCount").innerHTML = "<span class=\"notify-badge\">"+data.count+"</span>";
             document.getElementById(productid).innerHTML = data.productCount;
+            document.getElementById("totalPrice").value = data.totalprice;
         }
     };
-
     xhr.open("POST", "/Home/AddToCart?id=" + productid, true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf8");
     xhr.send();
@@ -47,7 +29,6 @@ function onClick2(event) {
     let elem = event.currentTarget;
     let productid = elem.getAttribute("productId");
     let row = elem.parentNode.parentNode.rowIndex;
-
 
     let xhr = new XMLHttpRequest();
 
@@ -68,9 +49,7 @@ function onClick2(event) {
             if (document.getElementById("table1").rows.length == 0) {
                 document.getElementById("cartlist").innerHTML = "<p>Shopping Cart is empty! Click Continue Shopping and Add Items to Cart!</p>";
                 document.getElementById("price").innerHTML = "";
-
             }
-
         }
     };
 
